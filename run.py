dcb9022d49b8cc824094ad7666b234ddadf15d3f
anusha2904 <60822709+anusha2904@users.noreply.github.com>
2022-04-10 12:00:01 +0530

    Added the rest of the tweets

--- run.py
+++ run.py
@@ -3,22 +3,23 @@ import json
 import time
 
 id_tweet_label_new = []
-f2 = open("id_tweet_label.txt", "w")
+f2 = open("id_tweet_label.txt", "a")
 f1 = open("all.txt", "r")
 
 all_d = f1.readlines()
 all_d = [x.strip().split(" ") for x in all_d]
 
-for i in all_d:
-  x = subprocess.run(["./run.sh", i[0]], capture_output=True)
-  y = (x.stdout).decode("utf-8")
-  z = json.loads(str(y))
-  print(z)
-  try:
-    s = z['data']
-    res = str(i[0]) + " "+ "\""+s['text']+"\""+" "+str(i[1]+"\n")
-    f2.write(res) 
-    time.sleep(2)
-  except:
-     print(i[0]," ERROR")
-     f2.write("ERROR\n")
+for i in range(len(all_d)):
+  if i > 7119:
+    x = subprocess.run(["./run.sh", all_d[i][0]], capture_output=True)
+    y = (x.stdout).decode("utf-8")
+    z = json.loads(str(y))
+    print(z)
+    try:
+      s = z['data']
+      res = str(all_d[i][0]) + " "+ "\""+s['text']+"\""+" "+str(all_d[i][1]+"\n")
+      f2.write(res) 
+      time.sleep(2)
+    except:
+       print(all_d[i][0]," ERROR")
+       f2.write("ERROR\n")
