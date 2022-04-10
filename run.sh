d72ee5b7786a47d4790ec1a12c8782a0bf1b7eb0
anusha2904 <60822709+anusha2904@users.noreply.github.com>
2022-04-10 08:59:48 +0530

    Adding shell script file to scrape tweets

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -X GET -H "Authorization: Bearer AAAAAAAAAAAAAAAAAAAAAID6bAEAAAAAfRXnT87MPzFEHAXz84%2BqXZNJHjw%3DOAKUZQmr8biGZ4hE7LwTW5zMqRuF44ZcRjdthePENYYziKYSHK" "https://api.twitter.com/2/tweets/$1" 
